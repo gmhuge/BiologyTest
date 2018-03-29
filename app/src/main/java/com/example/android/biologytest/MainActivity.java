4b92a8f0d5105434e9849c9b0dcc87e9103f5a41
gmhuge <gmhuge@gmail.com>
2018-03-30 01:53:51 +0300

    test

--- app/src/main/java/com/example/android/biologytest/MainActivity.java
+++ app/src/main/java/com/example/android/biologytest/MainActivity.java
@@ -1,9 +1,14 @@
 package com.example.android.biologytest;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.ScrollView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -46,5 +51,46 @@ public class MainActivity extends AppCompatActivity {
             quizStatus++;
             activateQuizView(quizStatus);
         }
+    }
+
+    public void submitQuiz(View view){
+
+        switch (view.getId()){
+            case R.id.quiz1submit:
+                RadioGroup radio1 = findViewById(R.id.radio1);
+                int checkedRadio1Id = radio1.getCheckedRadioButtonId();
+                Button buttonSubmit1 = findViewById(R.id.quiz1submit);
+                Button buttonNext1 = findViewById(R.id.next1);
+                if (checkedRadio1Id == R.id.radio1_2) {
+                    buttonSubmit1.setText("RIGHT");
+                    buttonSubmit1.setBackgroundColor(Color.GREEN);
+                    correctAnswers++;
+                }
+                else {
+                    buttonSubmit1.setText("WRONG");
+                    buttonSubmit1.setBackgroundColor(Color.RED);
+                }
+                buttonSubmit1.setClickable(false);
+                buttonNext1.setVisibility(View.VISIBLE);
+            break;
+            case R.id.quiz2submit:
+                RadioGroup radio2 = findViewById(R.id.radio2);
+                int checkedRadio2Id = radio2.getCheckedRadioButtonId();
+                Button buttonSubmit2 = findViewById(R.id.quiz2submit);
+                Button buttonNext2 = findViewById(R.id.next2);
+                if (checkedRadio2Id == R.id.radio2_1) {
+                    buttonSubmit2.setText("RIGHT");
+                    buttonSubmit2.setBackgroundColor(Color.GREEN);
+                    correctAnswers++;
+                }
+                else {
+                    buttonSubmit2.setText("WRONG");
+                    buttonSubmit2.setBackgroundColor(Color.RED);
+                }
+                buttonSubmit2.setClickable(false);
+                buttonNext2.setVisibility(View.VISIBLE);
+            break;
+        }
+
     }
 }
