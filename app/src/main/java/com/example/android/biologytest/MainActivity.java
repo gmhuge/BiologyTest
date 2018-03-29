abbb4623b469ad0e2b19f5fe3bb013785cba059e
gmhuge <gmhuge@gmail.com>
2018-03-29 21:48:43 +0300

    test

--- app/src/main/java/com/example/android/biologytest/MainActivity.java
+++ app/src/main/java/com/example/android/biologytest/MainActivity.java
@@ -7,26 +7,44 @@ import android.widget.ScrollView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final int MAX_QUIZ_NUMBER_VALUE = 4;
     int quizStatus = 0;
     int correctAnswers = 0;
+    int count;
 
+    int[] quizes = {R.id.quiz0,R.id.quiz1,R.id.quiz2,R.id.quiz3,R.id.quiz4};
+    ScrollView[] quizViews = new ScrollView[MAX_QUIZ_NUMBER_VALUE+1];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        initialize();
+    }
+
+    private void initialize() {
         quizStatus = 0;
         correctAnswers = 0;
 
+        for (count = 0; count <= MAX_QUIZ_NUMBER_VALUE; count++) {
+            quizViews[count] = findViewById(quizes[count]);
+        }
 
-
+        activateQuizView(0);
     }
 
+    private void activateQuizView(int number){
+        for (count = 0; count <= MAX_QUIZ_NUMBER_VALUE; count++) {
+            quizViews[count].setVisibility(View.GONE);
+        }
+        quizViews[number].setVisibility(View.VISIBLE);
+    }
 
-    private void setQuizStatus(int quizStatus) {
-
-        ScrollView quiz0 = findViewById(R.id.quiz0);
-        quiz0.setVisibility(View.VISIBLE);
+    public void nextQuiz(View view) {
+        if (quizStatus < MAX_QUIZ_NUMBER_VALUE){
+            quizStatus++;
+            activateQuizView(quizStatus);
+        }
     }
 }
